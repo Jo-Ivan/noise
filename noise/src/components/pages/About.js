7199de952746ef0ba1bb851e862ec6579d1f0316
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2021-09-14 16:18:15 -0400

    finish about page

--- noise/src/components/pages/About.js
+++ noise/src/components/pages/About.js
@@ -2,8 +2,12 @@ import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h1>About Noise</h1>
+    <div className="full-height">
+      <h1 className="is-size-1">About Noise</h1>
+      <p>
+        Noise is a React-based music journal app that allows you to search for any album in Discog's database. The user
+        will then be able to save the album into a favorites list and write a note about it.
+      </p>
     </div>
   );
 };
