910a87a00bf4cd34200efcdd153424d7241c8375
Jo-Ivan Melgarejo <joivan.melgarejo@gmail.com>
2021-09-14 15:51:11 -0400

    add full height for journal page

--- noise/src/components/journal/Journal.js
+++ noise/src/components/journal/Journal.js
@@ -4,7 +4,7 @@ import JournalCard from "./JournalCard";
 const Journal = ({ entries }) => {
   console.log(entries);
   return (
-    <div>
+    <div className="full-height">
       <h1 className="is-size-1">Journal Entries</h1>
       <div className="columns is-multiline">
         {entries.map((result) => {
--- noise/src/components/journal/JournalCard.js
+++ noise/src/components/journal/JournalCard.js
@@ -15,20 +15,6 @@ const JournalCard = ({ result }) => {
               <p className="title is-4">
                 {result.album} - {result.year}
               </p>
-              <p className="is-size-5 has-text-weight-medium">Genre:</p>
-              <ul>
-                {result.genre.map((element, idx) => {
-                  return <li key={idx}>{element}</li>;
-                })}
-              </ul>
-              <p className="is-size-5 has-text-weight-medium">Format:</p>
-              <ul>
-                {result.format.map((element, idx) => {
-                  return <li key={idx}>{element}</li>;
-                })}
-              </ul>
-              <p className="is-size-5 has-text-weight-medium">Country:</p>
-              <p>{result.country}</p>
             </div>
           </div>
         </div>
